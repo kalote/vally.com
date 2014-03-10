127c36feb5d475b5578dea70933c075228930c47
Johann BICH <johannbich@gmail.com>
2014-03-10 20:57:35 +0100

    import initial

---
+++ config/settings.inc.php
@@ -0,0 +1,18 @@
+<?php
+define('_DB_SERVER_', 'localhost');
+define('_DB_NAME_', '_amir');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheFs');
+define('_PS_CACHE_ENABLED_', '1');
+define('_MEDIA_SERVER_1_', '');
+define('_MEDIA_SERVER_2_', '');
+define('_MEDIA_SERVER_3_', '');
+define('_COOKIE_KEY_', '10puKvqv23ufrrp5U0xv1iwfO4VvsenaDf9nOSUMEfBCxh7he7WQjPKg');
+define('_COOKIE_IV_', 'DbTK4J3I');
+define('_PS_CREATION_DATE_', '2013-12-17');
+define('_PS_VERSION_', '1.5.6.1');
+define('_RIJNDAEL_KEY_', 'P6f7BHMU6ZUXTow9SAWdx841x7RvHdas');
+define('_RIJNDAEL_IV_', 'jOyVX4Jo90Z1PTL8UkAwfg==');
